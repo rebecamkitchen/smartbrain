08804ff82b808043cf435756b08a76dee0e61e9a
Rebeca Kitchen <rebecakitchen@Rebecas-MacBook-Pro.local>
2020-09-12 15:18:33 -0500

    uploading files

---
+++ build/precache-manifest.977f99ddfc37768b11d62049846853f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c46d2d35704c261c4ea0fa6a54c938b",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "b34696081b8d13104006",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b4fbe2160eece9e0c0fc",
+    "url": "/smartbrain/static/css/main.0af5f48d.chunk.css"
+  },
+  {
+    "revision": "b34696081b8d13104006",
+    "url": "/smartbrain/static/js/2.d2270ace.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain/static/js/2.d2270ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fbe2160eece9e0c0fc",
+    "url": "/smartbrain/static/js/main.306ca01a.chunk.js"
+  },
+  {
+    "revision": "ae456e4515232efad5b0",
+    "url": "/smartbrain/static/js/runtime-main.b715ec47.js"
+  }
+]);
